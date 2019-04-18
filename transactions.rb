5889fd59831eabe1fde4e85dba5c5791adc27e71
Ion Mocan <ionatanmocan@gmail.com>
2019-04-18 17:51:14 +0300

    fixed everything except rewriting xpath

--- transactions.rb
+++ transactions.rb
@@ -1,10 +1,10 @@
 class Transactions
   attr_accessor :date, :description, :amount, :currency
 
-  def initialize(date, description, amount, currency)
-    @date        = date
-    @description = description
-    @amount      = amount
-    @currency    = currency
+  def initialize(args = {})
+    @date        = args[:date]
+    @description = args[:description]
+    @amount      = args[:amount]
+    @currency    = args[:currency]
   end
 end
